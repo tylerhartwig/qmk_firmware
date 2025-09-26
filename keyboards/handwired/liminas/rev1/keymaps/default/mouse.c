1846cd3218fdb3f0607930cb12982ddd424e279a
Tyler Hartwig <tyler.hartwig@materialize.com>
2025-09-25 21:37:39 -0400

    Initial keyboard setup

---
+++ keyboards/handwired/liminas/rev1/keymaps/default/mouse.c
@@ -0,0 +1,3 @@
+//
+// Created by Tyler Hartwig on 9/25/25.
+//
---
+++ keyboards/handwired/liminas/rev1/keymaps/default/rules.mk
@@ -0,0 +1,15 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = yes
+RGBLIGHT_SUPPORTED = yes
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+
+# Trackball
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pwm3389
+
