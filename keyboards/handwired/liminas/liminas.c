b969b426530bc32acdeb97fcc546f0d800308994
Tyler Hartwig <tyler.hartwig@materialize.com>
2026-01-09 12:29:19 -0500

    working rgb for one half

--- keyboards/handwired/liminas/liminas.c
+++ keyboards/handwired/liminas/liminas.c
@@ -10,13 +10,23 @@
 #ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = {
     {
-        { 0, 1, 2, 3, 4, 5 }
+        { 0, 1, 2, 3, 4, 5 },
     },
     {
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 }, { 40, 0 }, { 50, 0 }
+        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
+        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
+        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
+        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
+        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
+        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 }
     },
     {
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT
     }
 };
 #endif
--- keyboards/handwired/liminas/mcuconf.h
+++ keyboards/handwired/liminas/mcuconf.h
@@ -2,13 +2,7 @@
 
 #include_next <mcuconf.h>
 
-// SPI for trackball
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 FALSE
 
 #undef RP_SPI_USE_SPI1
 #define RP_SPI_USE_SPI1 TRUE
 
-// PIO for WS2812 LEDs
-#undef RP_PWM_USE_PWM0
-#define RP_PWM_USE_PWM0 FALSE
