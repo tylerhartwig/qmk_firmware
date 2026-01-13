8450a8999ce1f31f3181b027e15869ae4b0540ee
Tyler Hartwig <tyler.hartwig@materialize.com>
2026-01-13 10:31:12 -0500

    fix thumb cluster positioning

--- keyboards/handwired/liminas/keymaps/default/keymap.c
+++ keyboards/handwired/liminas/keymaps/default/keymap.c
@@ -20,24 +20,6 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
-/*
-#define LAYOUT( \
-        L00, L01, L02, L03, L04, L05,            R00, R01, R02, R03, R04, R05, \
-        L06, L07, L08, L09, L10, L11,            R06, R07, R08, R09, R10, R11, \
-        L12, L13, L14, L15, L16, L17,            R12, R13, R14, R15, R16, R17, \
-        L18, L19, L20, L21, L22, L23,            R18, R19, R20, R21, R22, R23, \
-                  LT0, LT1, LT2, LT3,            RT1, RT2, RT3, RT4, \
-                  LT4,      LT5,                      RT5,      RT6 \
-    ) { \
-
-        L00, L01, L02, L03, L04, L05,            R00, R01, R02, R03, R04, R05, \
-        L06, L07, L08, L09, L10, L11,            R06, R07, R08, R09, R10, R11, \
-        L12, L13, L14, L15, L16, L17,            R12, R13, R14, R15, R16, R17, \
-        L18, L19, L20, L21, L22, L23,            R18, R19, R20, R21, R22, R23, \
-                  LT0, LT1, LT2, LT3,            RT1, RT2, RT3, RT4, \
-                  LT4,      LT5,                      RT5,      RT6 \
-}
-*/
 
 #include "quantum.h"
 #include "pointing_device.h"
--- keyboards/handwired/liminas/liminas.c
+++ keyboards/handwired/liminas/liminas.c
@@ -9,28 +9,53 @@
 
 #ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = {
+    // Key-to-LED matrix mapping
+    // Left half (rows 0-4), then right half (rows 5-9)
     {
-        { 0, 1, 2, 3, 4, 5 },
+        // Left half
+        {  0,  1,  2,  3,  4,  5 },
+        {  6,  7,  8,  9, 10, 11 },
+        { 12, 13, 14, 15, 16, 17 },
+        { 18, 19, 20, 21, 22, 23 },
+        { 24, 25, 26, 27, 28, 29 },
+        // Right half
+        { 30, 31, 32, 33, 34, 35 },
+        { 36, 37, 38, 39, 40, 41 },
+        { 42, 43, 44, 45, 46, 47 },
+        { 48, 49, 50, 51, 52, 53 },
+        { 54, 55, 56, 57, 58, 59 }
     },
+    // Physical LED positions (x, y) - range 0-224 for x, 0-64 for y
     {
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }, { 20, 0 }, { 30, 0 },
-        { 0,0 }, { 10, 0 }
+        // Left half (LEDs 0-29)
+        {  0,  0}, { 20,  0}, { 40,  0}, { 60,  0}, { 80,  0}, {100,  0},
+        {  0, 16}, { 20, 16}, { 40, 16}, { 60, 16}, { 80, 16}, {100, 16},
+        {  0, 32}, { 20, 32}, { 40, 32}, { 60, 32}, { 80, 32}, {100, 32},
+        {  0, 48}, { 20, 48}, { 40, 48}, { 60, 48}, { 80, 48}, {100, 48},
+		// Left Thumb Cluster
+        {101, 52}, {108, 54}, {104, 50}, {102, 60}, {111, 58}, {111, 64},
+        // Right half (LEDs 30-59)
+        {124,  0}, {144,  0}, {164,  0}, {184,  0}, {204,  0}, {224,  0},
+        {124, 16}, {144, 16}, {164, 16}, {184, 16}, {204, 16}, {224, 16},
+        {124, 32}, {144, 32}, {164, 32}, {184, 32}, {204, 32}, {224, 32},
+        {124, 48}, {144, 48}, {164, 48}, {184, 48}, {204, 48}, {224, 48},
+		// Right Thumb Cluster
+        {123, 52}, {116, 54}, {120, 50}, {122, 60}, {113, 58}, {113, 64}
     },
+    // LED flags (all 60)
     {
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
-		LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT
+        // Left half
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        // Right half
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT
     }
 };
 #endif
